6151d7a9374324fafb06ecc5955932d7aa4cd841
josetr <37419832+josetr@users.noreply.github.com>
2019-11-19 01:52:09 +0100

    Add support for VS2019

--- VsClassLibrary/VsClassLibrary.Tests/DteUtilitiesTests.cs
+++ VsClassLibrary/VsClassLibrary.Tests/DteUtilitiesTests.cs
@@ -28,5 +28,11 @@ namespace VsClassLibrary.Tests
         {
             Assert.Equal("15.0", DteUtilities.GetVsVersion());
         }
+
+        [VsFact(Version = "2019")]
+        void TestMethod16()
+        {
+            Assert.Equal("16.0", DteUtilities.GetVsVersion());
+        }
     }
 }
